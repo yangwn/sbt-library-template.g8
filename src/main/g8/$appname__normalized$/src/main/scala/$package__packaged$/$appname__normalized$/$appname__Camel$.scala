7e27c4a97a4a62dd723dbe9e2382ffae030327ac
wnyang <wunanyang@wnyangs-MacBook-Pro.local>
2019-01-30 10:10:47 +0800

    wunan

--- src/main/g8/$appname__normalized$/src/main/scala/$package__packaged$/$appname__normalized$/$appname__Camel$.scala
+++ src/main/g8/$appname__normalized$/src/main/scala/$package__packaged$/$appname__normalized$/$appname__Camel$.scala
@@ -3,13 +3,6 @@ package $package$.$appname;format="word"$
 object $appname;format="Camel"$ extends App{
 
   val appName = "TEST-APP"
-  val master = "local[4]"
-
-  val spark = SparkSession.builder.appName(appName).master(master).getOrCreate()
-
-  // read a file
-  val lines = spark.read.textFile("classpath:README.md")
-  val totalLength = lines.rdd.map(line => line.length).reduce((a, b) => a + b)
-  println(totalLength)
+  println(appName)
 
 }
